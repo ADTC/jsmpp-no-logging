6022fd35c99b526301ff052ec3d132c5451d67c1
ADTC <send2adtc@gmail.com>
2014-08-19 11:24:35 +0800

    Add pretentious logger classes
    Adds pretentious slf4j Logger and LoggerFactory classes to remove the
    Log4J/SLF4J dependency.

---
+++ jsmpp/src/main/java/org/slf4j/Logger.java
@@ -0,0 +1,29 @@
+package org.slf4j;
+
+public class Logger {
+
+	public void debug(Object... obj) {
+		//ignore
+	}
+
+	public void error(Object... obj) {
+		//ignore
+	}
+
+	public void error(String string, Throwable t) {
+		//ignore
+	}
+
+	public void info(Object... obj) {
+		//ignore
+	}
+
+	public void warn(Object... obj) {
+		//ignore
+	}
+
+	public void warn(String string, Throwable t) {
+		//ignore
+	}
+
+}
